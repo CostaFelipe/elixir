bb76c59628b90d661371efe6e6b41a5f931b5f28
Felipe Costa <costadefelipe@gmail.com>
2021-04-13 16:02:21 -0300

    init mix

---
+++ apptest/lib/apptest.ex
@@ -0,0 +1,18 @@
+defmodule Apptest do
+  @moduledoc """
+  Documentation for `Apptest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Apptest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
