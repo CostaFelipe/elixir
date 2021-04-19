ffc11a553836c93f959c7f0e05e8d56b5013c349
Felipe Costa <costadefelipe@gmail.com>
2021-04-19 14:32:19 -0300

    melhorando o code

--- bank/lib/conta.ex
+++ bank/lib/conta.ex
@@ -1,6 +1,4 @@
 defmodule Conta do
   defstruct usuario: Usuario, saldo: nil
- def cadastrar(usuario) do
-    %__MODULE__{usuario: usuario, saldo: 1000}
- end
+ def cadastrar(usuario), do: %__MODULE__{usuario: usuario, saldo: 1000}
 end
