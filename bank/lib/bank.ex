4a7c834d2c553fa4e991f66c88778a252eda2779
Felipe Costa <costadefelipe@gmail.com>
2021-04-27 18:56:13 -0300

    treinando command

--- bank/lib/bank.ex
+++ bank/lib/bank.ex
@@ -1,17 +1,4 @@
 defmodule Bank do
-  @moduledoc """
-  Documentation for `Bank`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bank.hello()
-      :world
-
-  """
   def hello do
     :world
   end
--- bank/lib/conta.ex
+++ bank/lib/conta.ex
@@ -1,4 +1,7 @@
 defmodule Conta do
   defstruct usuario: Usuario, saldo: 1000
  def cadastrar(usuario), do: %__MODULE__{usuario: usuario}
+ def transferir(contas, de, para, valor) do
+
+ end
 end
