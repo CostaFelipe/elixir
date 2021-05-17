59e84bd33acdd8296e84e485f44e4287d53bae3f
Felipe Costa <costadefelipe@gmail.com>
2021-05-17 08:06:21 -0300

    init

--- bank/lib/bank.ex
+++
@@ -1,5 +0,0 @@
-defmodule Bank do
-  def hello do
-    :world
-  end
-end
--- bank/lib/conta.ex
+++
@@ -1,7 +0,0 @@
-defmodule Conta do
-  defstruct usuario: Usuario, saldo: 1000
- def cadastrar(usuario), do: %__MODULE__{usuario: usuario}
- def transferir(contas, de, para, valor) do
-
- end
-end
