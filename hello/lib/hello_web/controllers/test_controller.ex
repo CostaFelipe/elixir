b70401401ca9a092e17c8033b0a0917e4a429bee
Felipe Costa <costadefelipe@gmail.com>
2021-06-13 08:47:16 -0300

    testando

--- hello/lib/hello_web/controllers/test_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule HelloWeb.TestController do
-  use HelloWeb, :controller
-
-  def  test(conn, _params)do
-    render(conn, "index.html")
-  end
-end
--- hello/lib/hello_web/views/test_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule HelloWeb.TestView do
-  use HelloWeb, :view
-end
