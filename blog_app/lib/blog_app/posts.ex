8405a125bb6c80992d5d5d27c2eed86875c09a78
Felipe Costa <costadefelipe@gmail.com>
2021-07-01 13:34:42 -0300

    created context, html posts

---
+++ blog_app/lib/blog_app/posts.ex
@@ -0,0 +1,33 @@
+defmodule BlogApp.Posts do
+
+  import Ecto.Query, warn: false
+  alias BlogApp.Repo
+
+  alias BlogApp.Posts.Post
+
+  def list_posts do
+    Repo.all(Post)
+  end
+
+  def get_post!(id), do: Repo.get!(Post, id)
+
+  def create_post(attrs \\ %{}) do
+    %Post{}
+    |> Post.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_post(%Post{} = post, attrs) do
+    post
+    |> Post.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_post(%Post{} = post) do
+    Repo.delete(post)
+  end
+
+  def change_post(%Post{} = post) do
+    Post.changeset(post, %{})
+  end
+end
